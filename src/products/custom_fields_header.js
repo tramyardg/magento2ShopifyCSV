6211950d549cbed5f67e58e223a3ab113ce793f3
raydguzman <rguzman@parkour3.com>
2021-04-06 14:32:29 -0500

    custom fields header

---
+++ src/products/custom_fields_header.js
@@ -0,0 +1,110 @@
+exports.custom_fields_header = [
+    {
+      id: "handle",
+      title: "Handle",
+    },
+    {
+      id: "title",
+      title: "Title",
+    },
+    {
+      id: "vendor",
+      title: "Vendor",
+    },
+    {
+      id: "published",
+      title: "Published",
+    },
+    {
+      id: "option1_name",
+      title: "Option1 Name",
+    },
+    {
+      id: "option1_value",
+      title: "Option1 Value",
+    },
+    {
+      id: "option2_name",
+      title: "Option2 Name",
+    },
+    {
+      id: "option2_value",
+      title: "Option2 Value",
+    },
+    {
+      id: "option3_name",
+      title: "Option3 Name",
+    },
+    {
+      id: "option3_value",
+      title: "Option3 Value",
+    },
+    {
+        id: "sku",
+        title: "SKU"
+    },
+    {
+      id: "variant_inventory_qty",
+      title: "Variant Inventory Qty",
+    },
+    {
+      id: "variant_inventory_tracker",
+      title: "Variant Inventory Tracker",
+    },
+    {
+      id: "variant_price",
+      title: "Variant Price",
+    },
+    {
+      id: "variant_requires_shipping",
+      title: "Variant Requires Shipping",
+    },
+    {
+      id: "variant_taxable",
+      title: "Variant Taxable",
+    },
+    {
+      id: "variant_fulfillment_service",
+      title: "Variant Fulfillment Service",
+    },
+    {
+      id: "variant_grams",
+      title: "Variant Grams",
+    },
+    {
+      id: "variant_weight_unit",
+      title: "Variant Weight Unit",
+    },
+    {
+      id: "status",
+      title: "Status",
+    },
+    {
+      id: "seo_title",
+      title: "SEO Title",
+    },
+    {
+      id: "gift_card",
+      title: "Gift Card",
+    },
+    {
+      id: "collection",
+      title: "Collection",
+    },
+    {
+      id: "image_src",
+      title: "Image Src",
+    },
+    {
+      id: "image_position",
+      title: "Image Position",
+    },
+    {
+      id: "p3_description_fr",
+      title: `custom_fields["p3_description_fr"]`
+    }
+  ];
+  
+  // custom_fields["sur-titre_du_produit"]	custom_fields["sur-titre_du_produit_-_en"]	custom_fields["p3_similar_product"]	custom_fields["p3_perfect_match"]	custom_fields["p3_description_fr"]
+  
+  
